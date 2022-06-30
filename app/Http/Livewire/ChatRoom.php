97e0c5ea1639b4f69bbd5d7ddf39155e13fc6e75
Victor Lima <victor.lima@devsquad.com>
2022-06-30 20:57:06 -0300

    Broadcast is working with Pusher

--- app/Http/Livewire/ChatRoom.php
+++ app/Http/Livewire/ChatRoom.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Livewire;
 
+use App\Events\MessageSent;
 use Livewire\Component;
 
 class ChatRoom extends Component
 {
+    public string $message = 'test';
+
+    protected $listeners = ['echo:messages,MessageSent' => 'showMessage'];
+
+    public function send()
+    {
+        event(new MessageSent);
+        $this->message = 'sending msg';
+    }
+
+    public function showMessage()
+    {
+        $this->message = 'changed by broadcast';
+    }
+
     public function render()
     {
         return view('livewire.chat-room');
